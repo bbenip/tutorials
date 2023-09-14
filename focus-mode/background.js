839f865e06833c80c938e60d7213bbb629905a25
Baljinder Benipal <bbenipal10@gmail.com>
2023-09-14 16:59:20 -0400

    Add badge text to display on/off state

--- focus-mode/background.js
+++ focus-mode/background.js
@@ -1,5 +1,17 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setBadgeText({
-    text: 'OFF',
-  });
+const extensionsDocsUrlPrefix = 'https://developer.chrome.com/docs/extensions';
+const webStoreDocsUrlPrefix = 'https://developer.chrome.com/docs/webstore';
+
+chrome.action.onClicked.addListener(async (tab) => {
+  if (
+    tab.url.startsWith(extensionsDocsUrlPrefix)
+    || tab.url.startsWith(webStoreDocsUrlPrefix)
+  ) {
+    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+
+    await chrome.action.setBadgeText({
+      tabId: tab.id,
+      text: nextState,
+    });
+  }
 });
