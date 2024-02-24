c0594fcb4ea4ab8f3099dde234e038dc12477c36
Baljinder Benipal <bbenipal10@gmail.com>
2024-02-24 14:22:01 -0500

    Remove exercism
    Exercism provides resources to learn programming languages and concepts.
    However, the content consists of programming challenges and it is
    inaccurate to designate the materials as a tutorial.

--- exercism/elixir/name-badge/lib/name_badge.ex
+++
@@ -1,8 +0,0 @@
-defmodule NameBadge do
-  def print(id, name, department) do
-    id_prefix = if id !== nil, do: "[#{id}] - ", else: ""
-    department_suffix = if department !== nil, do: String.upcase("#{department}"), else: "OWNER"
-
-    "#{id_prefix}#{name} - #{department_suffix}"
-  end
-end
